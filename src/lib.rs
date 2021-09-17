6dba45983e407009fac8d33a98831aedf3a84fa6
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-09-17 13:20:44 +0900

    replaced CreateThread with std::thread.
    in the DllMain function, I removed the CreateThread() function and instead use std::thread::spawn() for simplicity.

--- src/lib.rs
+++ src/lib.rs
@@ -1,44 +1,36 @@
-use bindings::Windows::Win32::Foundation::{CloseHandle, BOOL, HINSTANCE};
+use bindings::Windows::Win32::Foundation::{BOOL, HINSTANCE};
 use bindings::Windows::Win32::System::Console::{AllocConsole, FreeConsole};
 use bindings::Windows::Win32::System::LibraryLoader::DisableThreadLibraryCalls;
 use bindings::Windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
-use bindings::Windows::Win32::System::Threading::CreateThread;
-use bindings::Windows::Win32::System::Threading::THREAD_CREATION_FLAGS;
 use std::ffi::c_void;
-use std::mem;
-use std::ptr;
+use std::thread;
 
-extern "system" fn fmain(_lpthreadparameter: *mut c_void) -> u32 {
+macro_rules! create_entry_point {
+    () => {};
+}
+
+fn hack_main_thread() {
     unsafe {
         AllocConsole();
     }
-    for i in 0..10000 {
+    for i in 0..5000 {
         println!("{}", i);
     }
     unsafe {
         FreeConsole();
     }
-    0
 }
 
 #[no_mangle]
 #[allow(non_snake_case)]
-fn DllMain(h_module: *mut c_void, dw_reason: u32, _lp_reserved: *const c_void) -> BOOL {
+fn DllMain(h_module: HINSTANCE, dw_reason: u32, _: *const c_void) -> BOOL {
     if dw_reason == DLL_PROCESS_ATTACH {
         unsafe {
-            DisableThreadLibraryCalls(mem::transmute::<*mut c_void, HINSTANCE>(h_module));
-            let h_thread = CreateThread(
-                ptr::null_mut(),
-                0,
-                Some(fmain),
-                h_module,
-                THREAD_CREATION_FLAGS(0),
-                ptr::null_mut(),
-            );
-            if !h_thread.is_null() {
-                CloseHandle(h_thread);
-            }
+            DisableThreadLibraryCalls(h_module);
         }
+        thread::spawn(|| {
+            hack_main_thread();
+        });
     }
     BOOL(1)
 }
