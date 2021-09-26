0ac7dafc84a51e3f19249e1a0871073f28c1489e
s3pt3mb3r <s3pt3mb3r@protonmail.com>
2021-09-26 16:09:07 +0900

    deleted original code and use toy-arms

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,20 @@
-use bindings::Windows::Win32::Foundation::{BOOL, HINSTANCE};
 use bindings::Windows::Win32::System::Console::{AllocConsole, FreeConsole};
-use bindings::Windows::Win32::System::LibraryLoader::DisableThreadLibraryCalls;
-use bindings::Windows::Win32::System::SystemServices::DLL_PROCESS_ATTACH;
-use std::ffi::c_void;
-use std::thread;
 
+// A neat macro from toy-arms crate which eases defining entry point.
+toy_arms::create_entrypoint!(hack_main_thread);
+
+// A main function you want to run in the game.
 fn hack_main_thread() {
     unsafe {
         AllocConsole();
     }
 
     // YOUR STUNNING CODE'S SUPPOSED TO BE HERE;
+    for i in 0..30000 {
+        println!("Hello game {}", i);
+    }
 
     unsafe {
         FreeConsole();
     }
-}
-
-#[no_mangle]
-#[allow(non_snake_case)]
-extern "system" fn DllMain(h_module: HINSTANCE, dw_reason: u32, _: *const c_void) -> BOOL {
-    if dw_reason == DLL_PROCESS_ATTACH {
-        unsafe {
-            DisableThreadLibraryCalls(h_module);
-        }
-        thread::spawn(|| {
-            hack_main_thread();
-        });
-    }
-    BOOL(1)
 }
